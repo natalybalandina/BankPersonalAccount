4bce38545d0d84bb8761fadb402552e7db0e6a5b
Nataly Balandina <bal1nataly@gmail.com>
2025-06-15 01:46:09 +0300

    next function

--- src/processing.py
+++ src/processing.py
@@ -2,7 +2,7 @@ from typing import List, Dict
 from datetime import datetime
 
 
-def filter_by_state(id_numbers: str, state='EXECUTED') -> Dict[str, str]:
+def filter_by_state(id_numbers: str, state='EXECUTED') -> List[Dict]:
 
     """
  Принимает список словарей filter_by_state и значение для ключа state по умолчанию'EXECUTED'.
@@ -28,6 +28,18 @@ def filter_by_state(id_numbers: str, state='EXECUTED') -> Dict[str, str]:
 
     return (filter_default_key, filter_specified_state)
 
+
+def sort_by_date(data: List[Dict], reverse: bool = True) -> List[Dict]:
+    """
+Сортирует список операций по ключу 'date' (по убыванию) и возвращает новый список, отсортированный
+по дате.
+    """
+    return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=reverse)
+
+
+
+
+# Ожидаемый результат (сортировка по ключу 'state'):
 list_of_test = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                 {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
                 {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
@@ -38,3 +50,10 @@ EXECUTED, CANCELED = filter_by_state(list_of_test)
 print("Список с указынным значением 'state':", EXECUTED)
 
 print(f"Список с другим значением 'state':", CANCELED)
+
+
+# Ожидаемый результат (сортировка по убыванию даты):
+sorted_data_desc = sort_by_date(list_of_test)
+print(sorted_data_desc)
+
+
